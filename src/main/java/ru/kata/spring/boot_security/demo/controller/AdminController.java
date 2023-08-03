1bfb74f94e6fab7ffd58f339b22c40d68d651b8e
MaximButmanov <butmanovmax@gmail.com>
2023-08-03 19:20:07 +0300

    added unique username, update encoder, added UserController

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.security.NameValidator;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
@@ -13,71 +14,70 @@ import javax.validation.Valid;
 
 @Controller
 public class AdminController {
-    private final RoleService rolesServiceImp;
-    private final UserService usersServiceImp;
+    private final RoleService roleService;
+    private final UserService userService;
+    private final NameValidator nameValidator;
+
 
     @Autowired
-    public AdminController(RoleService roleService, UserService userService) {
-        this.rolesServiceImp = roleService;
-        this.usersServiceImp = userService;
+    public AdminController(RoleService roleService, UserService userService, NameValidator nameValidator) {
+        this.roleService = roleService;
+        this.userService = userService;
+        this.nameValidator = nameValidator;
     }
 
     @GetMapping("/admin/user")
     public String showAllUsers(Model model) {
-        model.addAttribute("user", usersServiceImp.findAll());
+        model.addAttribute("user", userService.findAll());
         return "all_users";
     }
 
     @GetMapping("/admin/{id}")
     public String showOneUser(@PathVariable("id") int id, Model model) {
-        model.addAttribute("user", usersServiceImp.findOne(id));
+        model.addAttribute("user", userService.findOne(id));
         return "show";
     }
 
     @GetMapping("/admin/new")
     public String showPageCreatingUser(Model model) {
         model.addAttribute("user", new User());
-        model.addAttribute("role", rolesServiceImp.getRoles());
+        model.addAttribute("role", roleService.getRoles());
         return "new";
     }
 
     @PostMapping("/admin/user")
     public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
-       // newUserValidator.validate(user, bindingResult);
+       nameValidator.validate(user, bindingResult);
         if (bindingResult.hasErrors()) {
             return "new";
         }
-        usersServiceImp.save(user);
+        userService.save(user);
         return "redirect:/admin/user";
     }
 
     @GetMapping("/admin/{id}/update")
     public String showPageEditUser(Model model, @PathVariable("id") int id) {
-        model.addAttribute("user", usersServiceImp.findOne(id));
-        model.addAttribute("role", rolesServiceImp.getRoles());
+        model.addAttribute("user", userService.findOne(id));
+        model.addAttribute("role", roleService.getRoles());
         return "update";
     }
 
     @PatchMapping("/admin/{id}")
     public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        nameValidator.validate(user, bindingResult);
         if (bindingResult.hasErrors()) {
             return "update";
         }
-        usersServiceImp.update(user);
+        userService.update(user);
         return "redirect:user";
     }
 
     @DeleteMapping("/admin/{id}")
     public String delete(@PathVariable("id") int id) {
-        usersServiceImp.delete(id);
+        userService.delete(id);
         return "redirect:user";
     }
-    @GetMapping("/user")
-    public String showUserInfo(Model model) {
-        User user = usersServiceImp.findOne();
-        model.addAttribute("user", user);
-        return "user";
-    }
+
     @GetMapping("/")
     private String showWelcomePage() {
         return "welcome";
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,15 +1,16 @@
 package ru.kata.spring.boot_security.demo.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.configs.WebSecurityConfig;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 import ru.kata.spring.boot_security.demo.security.UserDetailsSecurity;
@@ -20,12 +21,15 @@ import java.util.Optional;
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    public void context(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext; }
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        //this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> findAll() {
@@ -44,13 +48,17 @@ public class UserService implements UserDetailsService {
 
     @Transactional
     public void save(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(applicationContext.getBean(WebSecurityConfig.class)
+                        .passwordEncoder().encode(user
+                                .getPassword()));
         userRepository.save(user);
     }
 
     @Transactional
     public void update(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(applicationContext.getBean(WebSecurityConfig.class)
+                .passwordEncoder().encode(user
+                        .getPassword()));
         userRepository.save(user);
 
     }
